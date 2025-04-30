3c9de4afa286d6e762150fcfa344442f9d703c4d
Omar-Yasser-Frontend <craft14716@gmail.com>
2025-04-30 23:23:14 +0300

    fix api calls endpoints

--- client/src/utils/authApi.js
+++ client/src/utils/authApi.js
@@ -1,13 +1,10 @@
 export async function register(userData) {
-  const res = await fetch(
-    "https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/register",
-    {
-      method: "POST",
-      credentials: "include",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(userData),
-    }
-  );
+  const res = await fetch("https://pomo-server.vercel.app/api/register", {
+    method: "POST",
+    credentials: "include",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(userData),
+  });
 
   if (!res.ok) throw new Error("Failed to create user");
 
@@ -16,15 +13,12 @@ export async function register(userData) {
 }
 
 export async function login({ gmail, password }) {
-  const res = await fetch(
-    `https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/login`,
-    {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      credentials: "include",
-      body: JSON.stringify({ email: gmail, password }),
-    }
-  );
+  const res = await fetch(`https://pomo-server.vercel.app/api/login`, {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({ email: gmail, password }),
+  });
   if (!res.ok) throw new Error("User not found");
 
   const data = await res.json();
@@ -33,13 +27,10 @@ export async function login({ gmail, password }) {
 
 export async function logout() {
   try {
-    const res = await fetch(
-      "https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/logout",
-      {
-        method: "POST",
-        credentials: "include",
-      }
-    );
+    const res = await fetch("https://pomo-server.vercel.app/api/logout", {
+      method: "POST",
+      credentials: "include",
+    });
     if (!res.ok) throw new Error("Failed to logout");
 
     return null;
@@ -48,15 +39,12 @@ export async function logout() {
   }
 }
 export async function confirmation(code) {
-  const res = await fetch(
-    "https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/confirmation",
-    {
-      method: "POST",
-      credentials: "include",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({ code: Number(code) }),
-    }
-  );
+  const res = await fetch("https://pomo-server.vercel.app/api/confirmation", {
+    method: "POST",
+    credentials: "include",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({ code: Number(code) }),
+  });
   if (!res.ok) throw new Error("Failed to confirm email");
 
   return res.text();
--- client/src/utils/tasksApi.js
+++ client/src/utils/tasksApi.js
@@ -5,12 +5,9 @@ export async function getTasks() {
 
   if (login !== false) return null;
 
-  const res = await fetch(
-    `https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/tasks`,
-    {
-      credentials: "include",
-    }
-  );
+  const res = await fetch(`https://pomo-server.vercel.app/api/tasks`, {
+    credentials: "include",
+  });
   if (!res.ok) throw new Error("Failed to fetch data");
 
   let data = await res.json();
@@ -19,17 +16,14 @@ export async function getTasks() {
 }
 
 export async function addTask(task) {
-  const res = await fetch(
-    "https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/tasks",
-    {
-      method: "POST",
-      credentials: "include",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify({
-        task,
-      }),
-    }
-  );
+  const res = await fetch("https://pomo-server.vercel.app/api/tasks", {
+    method: "POST",
+    credentials: "include",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify({
+      task,
+    }),
+  });
   if (!res.ok) throw new Error("Failed to fetch data");
 
   const data = await res.json();
@@ -42,14 +36,11 @@ export async function deleteTask(_id) {
 
   if (login !== false) return null;
 
-  const res = await fetch(
-    `https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/tasks/${_id}`,
-    {
-      method: "DELETE",
-      credentials: "include",
-      headers: { "content-type": "application/json" },
-    }
-  );
+  const res = await fetch(`https://pomo-server.vercel.app/api/tasks/${_id}`, {
+    method: "DELETE",
+    credentials: "include",
+    headers: { "content-type": "application/json" },
+  });
   if (!res.ok) throw new Error("Failed to delete this task");
 
   const data = await res.json();
@@ -60,15 +51,12 @@ export async function updateTask({ _id, task }) {
   const login = isLogin();
 
   if (login !== false) return null;
-  const res = await fetch(
-    `https://3afde1ea-9062-4673-bedf-0d7f10f7f39c-00-2ziyswb0pl2vu.spock.replit.dev:8000/api/tasks/${_id}`,
-    {
-      method: "PUT",
-      credentials: "include",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(task),
-    }
-  );
+  const res = await fetch(`https://pomo-server.vercel.app/api/tasks/${_id}`, {
+    method: "PUT",
+    credentials: "include",
+    headers: { "content-type": "application/json" },
+    body: JSON.stringify(task),
+  });
 
   if (!res.ok) throw new Error("Failed to update task");
 
